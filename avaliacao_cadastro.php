d817bdcd9b7519347350c5189887e6c7c0994035
WrenchEscobar <ju_pn2010@hotmail.com>
2019-08-25 21:05:23 -0300

    Update avaliacao_cadastro.php

--- avaliacao_cadastro.php
+++ avaliacao_cadastro.php
@@ -56,7 +56,7 @@
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-	<title><?php echo $acao; ?> Prova - Prove</title>
+	<title><?php echo $acao; ?>Soluz</title>
 </head>
 
 <body>
